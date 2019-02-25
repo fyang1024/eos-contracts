d8e36287be50aaff9c8fbd885f03b99081869d5d
fyang1024 <fyang1024@gmail.com>
2019-02-25 11:58:09 +1100

    hello

---
+++ hello/hello.cpp
@@ -0,0 +1,15 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("hello")]] hello: public contract {
+    public:
+        using contract::contract;
+
+        [[eosio::action]]
+        void hi(name user) {
+            print("Hello, ", name{user});
+        }
+};
+
+EOSIO_DISPATCH( hello, (hi))
